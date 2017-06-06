4eae56c3df11a0b9ba0ef061cf68e7ae2d4b3d2c
rayallasureshkumar <rayallasureshkumar@gmail.com>
2017-06-06 13:50:12 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeatApp::Application.config.secret_token = 'd02df8cbe03ccdca480c8ee6c8f5d64fae97cf225adfcb9f87ab17b831c5702141a289b8205afe3f947c0a096241b6eed74139e3dba97c0fc9e78775c087ebce'
